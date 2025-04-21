b8fa1cf1c0de14c323106555c13e48bb1702d47b
Umang Garg <73619923+gargumang411@users.noreply.github.com>
2025-04-21 15:06:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+try:
+    __import__('pysqlite3')
+    import sys
+    sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+except KeyError:
+    pass  # fallback to default sqlite3
 import os
 import streamlit as st
 import numpy as np
